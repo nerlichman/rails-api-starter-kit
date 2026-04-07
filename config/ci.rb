d6741db03ce7befc3b3011c36a5c0e57378c4eda
Nicolas Erlichman <nicolas@gogrow.dev>
2026-04-07 15:37:07 -0300

    Use rpsec in bin/ci

--- config/ci.rb
+++ config/ci.rb
@@ -9,8 +9,7 @@ CI.run do
 
   step "Security: Gem audit", "bin/bundler-audit"
   step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
-  step "Tests: Rails", "bin/rails test"
-  step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
+  step "Tests: Rails", "bin/rails db:test:prepare spec"
 
   # Optional: set a green GitHub commit status to unblock PR merge.
   # Requires the `gh` CLI and `gh extension install basecamp/gh-signoff`.
